430c1aa889a5f14c5821f9c8f458815292e2148e
Gabriel Perdue <perdue@fnal.gov>
2013-11-12 14:44:46 -0600

    Add a script to minimize the packages used when working with older versions of vim. There has to be a better way to do this...

---
+++ clean_for_old_vim.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+rm -rf vim/bundle/haskellmode-vim/
+rm -rf vim/bundle/html-template-syntax/
+rm -rf vim/bundle/jshint/
+rm -rf vim/bundle/neco-ghc/
+rm -rf vim/bundle/tlib_vim/
+rm -rf vim/bundle/vim2hs
+rm -rf vim/bundle/vim-addon-mw-utils/
+rm -rf vim/bundle/vimerl/
+rm -rf vim/bundle/vim-snipmate/
+rm -rf vim/bundle/vim-snippets/
+
+
